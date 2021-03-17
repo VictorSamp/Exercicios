befa25d47b2addf03b3b9142c567a410c44b0e95
VictorSamp <victor.msampaio@outlook.com.br>
2021-03-17 09:54:23 -0300

    Inserindo exercício 'ValidacaoDeNota'

---
+++ ValidacaoDeNota/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace ValidacaoDeNota
+{
+    class Program
+    {
+        static List<double> notasValidas = new List<double>();
+
+        static void Main(string[] args)
+        {
+            do
+            {
+                double nota = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                ValidarNota(nota);
+            } while (notasValidas.Count < 2);
+
+            var media = CalcularMedia(notasValidas);
+
+            Console.WriteLine("media = {0}", media);
+        }
+
+        static void ValidarNota(double nota)
+        {
+            if (nota < 0 || nota > 10.0)
+            {
+                Console.WriteLine("nota invalida");
+            }
+            else
+            {
+                notasValidas.Add(nota);
+            }
+        }
+
+        static string CalcularMedia(List<double> notasValidas)
+        {
+            return (notasValidas.Sum() / notasValidas.Count)
+                .ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
