1cd97c84517bda5f4ed858a9f6d2e2889de69071
VictorSamp <victor.msampaio@outlook.com.br>
2021-04-27 12:19:41 -0300

    feat: adiciona desafio Area do Circulo

---
+++ AreaDoCirculo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace AreaDoCirculo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            double pi = 3.14159, raio;
+
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double area = pi * Math.Pow(raio, 2);
+
+            Console.WriteLine("A={0}", area.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
